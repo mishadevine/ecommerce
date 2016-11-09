82e84da71ab9ee213d05f7ea7c8616c0ba98da88
Misha Devine <mdevine@fullsail.edu>
2016-11-08 19:31:24 -0500

    store page semi done; changed nav items around; changed store.html to be homepage and added checkout.html

--- public/assets/js/mainfirebase.js
+++ public/assets/js/mainfirebase.js
@@ -1,14 +1,27 @@
 angular.module ("TatianasOldies", ["firebase","ngRoute","ngMessages"])
-  .config(["$routeProvider", function($routeProvider) {
-    $routeProvider.when("/", {
-      templateUrl: "parts/home.html",
-    }).when("/store", {
-      templateUrl: "parts/store.html",
-    }).when("/cart", {
-      templateUrl: "parts/cart.html",
-    })
-    .otherwise("/");
-  }])
-  .controller("MasterCtrl", function() {
+    .config(["$routeProvider", function($routeProvider) {
+      $routeProvider.when("/", {
+        templateUrl: "parts/store.html",
+        controller: "MasterCtrl",
+      }).when("/cart", {
+        templateUrl: "parts/cart.html",
+        controller: "MasterCtrl",
+      }).when("/checkout", {
+        templateUrl: "parts/checkout.html",
+      })
+      .otherwise("/");
+    }])
+    .controller("MasterCtrl", function($scope,$firebaseArray,$firebaseObject) {
+      var ref = new Firebase("https://tatianasoldskool.firebaseio.com/") // connecting to the database
+      var URL = "https://tatianasoldskool.firebaseio.com/store-items/cds";
+      var cdItemsRef = new Firebase(URL);
+      cdItems = $firebaseArray(cdItemsRef);
+      $scope.cdItems = cdItems;
 
-  })
+      // var ref = new Firebase("https://tatianasoldskool.firebaseio.com/") // connecting to the database
+      var URL = "https://tatianasoldskool.firebaseio.com/store-items/games";
+      var gameItemsRef = new Firebase(URL);
+      gameItems = $firebaseArray(gameItemsRef);
+      $scope.gameItems = gameItems;
+
+    })
